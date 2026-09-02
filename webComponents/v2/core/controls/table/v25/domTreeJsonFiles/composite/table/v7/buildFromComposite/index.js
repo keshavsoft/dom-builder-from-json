9352950d5140c7c52889f3f1fbc7e96f3f80cb43
keshavsoft <nknnkumar@yahoo.com>
2026-09-02 15:10:18 +0530

    tb8

---
+++ webComponents/v2/core/controls/table/v25/domTreeJsonFiles/composite/table/v7/buildFromComposite/index.js
@@ -0,0 +1,17 @@
+import findTargetNodeByPathParts from "./findTargetNodeByPathParts.js";
+import buildNodeSpecTree from "./buildNodeSpecTree.js";
+import applyThemeClassesToTree from "./applyThemeClassesToTree.js";
+import applySingleStepToSpecTree from "./applySingleStepToSpecTree.js";
+import applySequentialStepsToSpecTree from "./applySequentialStepsToSpecTree.js";
+import buildBaseSpecTreeFromComposite from "./buildBaseSpecTreeFromComposite.js";
+
+export {
+    findTargetNodeByPathParts,
+    buildNodeSpecTree,
+    applyThemeClassesToTree,
+    applySingleStepToSpecTree,
+    applySequentialStepsToSpecTree,
+    buildBaseSpecTreeFromComposite
+};
+
+export default buildBaseSpecTreeFromComposite;
--- webComponents/v2/core/controls/table/v25/domTreeJsonFiles/composite/table/v8/index.js
+++ webComponents/v2/core/controls/table/v25/domTreeJsonFiles/composite/table/v8/index.js
@@ -1,27 +1,28 @@
 import startCompositeDefinition from "./start.json" with { type: "json" };
+import specTemplatesDictionary from "./spec.json" with { type: "json" };
 import stepsDefinitionList from "./steps.json" with { type: "json" };
-import defaultThemedSpec from "./templates.js";
 import buildBaseSpecTreeFromComposite, { applySequentialStepsToSpecTree } from "./buildFromComposite.js";
 
 const initialBaseSpecTree = buildBaseSpecTreeFromComposite({
     inCompositeDef: startCompositeDefinition,
-    inTemplates: defaultThemedSpec
+    inTemplates: specTemplatesDictionary
 });
 
 const finalHydratedSpecTree = applySequentialStepsToSpecTree({
     inSpecTree: initialBaseSpecTree,
     inStepsDef: stepsDefinitionList,
-    inTemplates: defaultThemedSpec,
-    inEnableLog: false
+    inTemplates: specTemplatesDictionary,
+    inEnableLog: true
 });
 
 console.log("\n=====================================================================");
-console.log("[v6 Self-Contained Result] Final Children Tree:");
+console.log("[v8 Story-Subfolders Result] Final Children Tree:");
 console.log("=====================================================================");
 console.log(JSON.stringify(finalHydratedSpecTree.children, null, 2));
 
 export {
     startCompositeDefinition,
+    specTemplatesDictionary,
     stepsDefinitionList,
     initialBaseSpecTree,
     finalHydratedSpecTree,
